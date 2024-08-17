450bc2aa0a854a9690ed8ba561660a2a5453bcc0
cabitibaly <bitibalyclementalex@gmail.com>
2024-08-17 11:12:37 +0100

    recherche okay

--- src/components/inputSearch.jsx
+++ src/components/inputSearch.jsx
@@ -1,19 +1,53 @@
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CagnoteCard from "./cagnoteCard";
-import handicap from '../assets/image/handicap.jpg';
+// import handicap from '../assets/image/handicap.jpg';
 
 
 const InputSearch = () => {
-    const [text, setText] = useState('');
+  const [terme, setTerme] = useState('');
+  const [cagnottes, setCagnottes] = useState([]);
 
-    const width = `${(30/100) * 100}%`;
+    // const width = `${(30/100) * 100}%`;
 
-    const handleChange = (e) => {
-      setText(e.target.value);
+  const getCagnottes = async () => {
+      const res = await fetch('http://127.0.0.1:8000/api/cagnottes/', {
+          method: 'GET',
+          headers: {
+              'Content-Type' : 'application/json',
+          },
+      });
+      const data = await res.json();
+      if(data.status == 200) {
+          setCagnottes(data.message);
+      } else {
+          window.location.reload();
+      }
+  }
+
+  const recherche = async (e) => {
+      e.preventDefault();
+      setTerme(e.target.value);
+      const res = await fetch('http://127.0.0.1:8000/api/cagnottes/search/'+terme, {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+    });
+    const data = await res.json();
+    console.log(data);
+    if(data.status == 200) {
+        setCagnottes(data.message);
+    } else {
+        // window.location.reload();
     }
+  }
 
-    console.log(text)
+  useEffect(() => {
+
+      getCagnottes();
+
+  }, []);
 
     return (
       <>
@@ -25,38 +59,26 @@ const InputSearch = () => {
               type="text" 
               className="w-full bg-transparent text-lg p-1 outline-none placeholder:text-gray-600" 
               placeholder="Rechercher" 
-              onChange={handleChange}
+              onChange={recherche}
             />
           </div>
           <div className="p-4 md:px-16 w-full grid grid-cols-2 items-center justify-center gap-4 max-750:flex max-750:flex-col lg:grid-cols-3 lg:w-full">
-            <CagnoteCard 
-              url={'/cagnote/detail/id'} 
-              image={handicap}
-              titre={'Senior health malade Senior health malade'} 
-              pourcentage={width}
-              montantRecolte={'19 187'}
-            /> 
-            <CagnoteCard 
-              url={'/cagnote/detail/id'} 
-              image={handicap}
-              titre={'Senior health malade Senior health malade'} 
-              pourcentage={width}
-              montantRecolte={'19 187'}
-            /> 
-            <CagnoteCard 
-              url={'/cagnote/detail/id'} 
-              image={handicap}
-              titre={'Senior health malade Senior health malade'} 
-              pourcentage={width}
-              montantRecolte={'19 187'}
-            /> 
-            <CagnoteCard 
-              url={'/cagnote/detail/id'} 
-              image={handicap}
-              titre={'Senior health malade Senior health malade'} 
-              pourcentage={width}
-              montantRecolte={'19 187'}
-            /> 
+                { cagnottes.length > 0 ?
+                    cagnottes.map((cagnotte, index) => (
+                        <CagnoteCard 
+                            key={index}
+                            url={'/cagnote/detail/'+cagnotte.id}
+                            image={`http://127.0.0.1:8000${cagnotte.image_url}`}
+                            titre={cagnotte.intitule} 
+                            pourcentage={` ${(cagnotte.montant_collecte / cagnotte.objectif_montant_vise) * 100}% `}
+                            montantRecolte={cagnotte.montant_collecte}
+                        /> 
+                    )) 
+                    :
+                    <div className="border border-solid border-black p-4 h-44 flex items-center justify-center">
+                        <p className="font-bold text-xl text-center text-dark">Aucune cagnote trouvée avec le terme &quot;{terme}&quot;</p>
+                    </div>
+                }
           </div>
         </section>
       </>
